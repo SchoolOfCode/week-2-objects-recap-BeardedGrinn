ba5cc12c225ec8cfa9fb1f407404fbce1b365aa9
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-03 23:01:53 +0000

    Testing
    Tested true false change.
    Tried to teach girlfriend about my code, so i could
    practice explaining it simply.

---
+++ main.js
@@ -0,0 +1,63 @@
+const myCat = {
+    color: "black",
+    name: "Tony",
+    lovesCuddles: true,
+};
+
+console.log (myCat);
+// shows properties of my catch.
+
+const myHouse = {
+    stories: 2,
+    parking: false,
+    bedrooms: 2,
+    garden: true,
+    energyRating: "D",
+};
+
+console.log (myHouse);
+// shows properties of my house.
+
+const myCar = {
+    color: "red",
+    numberOfWheels: 4,
+    powerSteering: true,
+    numberOfSeats: 2,
+    milesPerGallon: 20,
+};
+
+console.log (myCar);
+// shows properties of my car.
+
+const myFavouriteFilm = {
+    title: "The Dark Knight",
+    releaseDate: 2008,
+    leadActor: "Christian Bale",
+};
+
+console.log (myFavouriteFilm);
+// shows details of my favorite film.
+
+let person = {
+    name: "Ben",
+    likesChocolate: true,
+  };
+
+  console.log (person.likesChocolate);
+  // calls to a specific value within person object.
+
+  if (person.likesChocolate) {
+    console.log ("Ben loves chocolate");
+    } else {
+        console.log ("Ben hates chocolate")
+    } 
+// if and else statement to call different outcome to console based on true or false.
+
+let desiredPleasantry = "greeting";
+
+let bensPhrases = {
+  greeting: "Well hello there!",
+  farewell: "See you in another life, brother",
+  smallTalk: "Warm today, isn't it...",
+};
+
